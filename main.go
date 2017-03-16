4bf7d72c56c629e5fd791393af28326217f72621
James K <james@gtoet.com>
2017-03-17 08:08:48 +1300

    Started adding translation of Models

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/emicklei/proto"
 	"github.com/jamesk/apidoc-proto/apidoc"
+	"strings"
+	"errors"
 )
 
 func main() {
@@ -52,6 +54,91 @@ func getProtoFromAPISpec(spec apidoc.Spec) proto.Proto {
 		pFile.Elements = append(pFile.Elements, &pEnum)
 	}
 
+	//Models
+	for _, aModel := range spec.Models {
+		pMessage := proto.Message{Name: aModel.Name}
+		for i, aField := range aModel.Fields {
+
+			//Apidoc types: boolean, date-iso8601, date-time-iso8601, decimal, double, integer, long, object, string, unit, uuid
+
+			pMessage.Elements = append(
+				pMessage.Elements,
+				&proto. {Name:aValue.Name, Integer:i},
+			)
+		}
+		pFile.Elements = append(pFile.Elements, &pMessage)
+	}
+
+	//Unions
+
+	//Resources
+
 	return pFile
 }
 
+func getProtoFieldFromApidoc(aField apidoc.Field, sequenceNumber int) (proto.Visitee, error) {
+	//Apidoc types: boolean, date-iso8601, date-time-iso8601, decimal, double, integer, long, object, string, unit, uuid
+	if strings.HasPrefix(aField.FieldType, "map[") {
+		return getMapProtoFieldFromApidoc(aField, sequenceNumber)
+	}
+
+	getNormalProtoFieldFromApidoc(aField, sequenceNumber)
+}
+
+func getNormalProtoFieldFromApidoc(aField apidoc.Field, sequenceNumber int) (proto.NormalField, error) {
+	pField := proto.NormalField{}
+
+	fieldType := aField.FieldType
+	if strings.HasPrefix(fieldType, "[") {
+		if !strings.HasSuffix(fieldType, "]") {
+			return pField, errors.New("Invalid type, starts with a [ but does not end with one")
+		}
+
+		pField.Repeated = true
+		fieldType = fieldType[1:len(fieldType)-1]
+	}
+
+	pType, err := getProtoTypeFromBasicApidocType(fieldType)
+	if err != nil {
+		return pField, err
+	}
+	pField.Type = pType
+
+	pField.Sequence = sequenceNumber
+
+	return pField, nil
+}
+
+func getMapProtoFieldFromApidoc(aField apidoc.Field) (proto.MapField, error) {
+	return proto.MapField{}, errors.New("Map fields are unsupported at the moment")
+}
+
+func getProtoTypeFromBasicApidocType(basicType string) (string, error) {
+	switch basicType {
+	case "boolean":
+		return "bool", nil
+	case "date-iso8601":
+		return "string", nil
+	case "date-time-iso8601":
+		return "string", nil
+	case "decimal":
+		return "", errors.New("Cannot translate decimal field types to proto type")
+	case "double":
+		return "double", nil
+	case "integer":
+		return "int32", nil
+	case "long":
+		return "int64", nil
+	case "object":
+		return "", errors.New("Cannot translate object field types to proto type")
+	case "string":
+		return "string", nil
+	case "unit":
+		return "", errors.New("Cannot translate unit field types to proto type")
+	case "uuid":
+		return "string", nil
+	default:
+		//Custom type or wrong type
+		return basicType, nil
+	}
+}
